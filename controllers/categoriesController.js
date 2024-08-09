f6f68ed555dde1ce163619385030cb7d62ab41bf
emiroquai <22440776+emiroquai@users.noreply.github.com>
2024-08-09 16:19:07 +0200

    Add categories styling

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -3,10 +3,10 @@ const { links } = require("./indexController");
 
 //Dummy categories -- to be deleted
 const categories = [
-  { href: "comedy", text: "Comedy" },
-  { href: "action", text: "Action" },
-  { href: "drama", text: "Drama" },
-  { href: "sci-fi", text: "Sci-Fi" },
+  { href: "comedy", text: "Comedy", imageUrl: "https://sp-ao.shortpixel.ai/client/to_auto,q_lossy,ret_img/https://my.kumonglobal.com/wp-content/uploads/2022/03/Learn-from-Rowan-Atkinson_Kumon-Malaysia_530x530_NewsThumbnail.jpg" },
+  { href: "action", text: "Action", imageUrl: "https://www.syfy.com/sites/syfy/files/2019/05/john-wick-2017-image-3840x2400.jpg" },
+  { href: "drama", text: "Drama", imageUrl: "https://nonstopnerd.com/wp-content/uploads/2023/07/or1.jpg" },
+  { href: "sci-fi", text: "Sci-Fi", imageUrl: "https://www.indiewire.com/wp-content/uploads/2014/12/interstellar-tesseract.jpg?w=2048&h=1494&crop=1" },
 ];
 
 async function getCategories(req, res) {
